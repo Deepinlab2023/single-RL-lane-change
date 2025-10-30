cb86745307d496113676f1cb73d3bef18fc7d6a7
Mawunge <mat.14.mt@gmail.com>
2025-10-30 10:18:24 -0400

    Modification of env training parameters

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
 
     elif args.env.lower() == 'sumo':
         env_name = 'sumo-v0'
-        env = SumoEnv(max_steps=20,render_mode='human' ,step_length=0.2,decision_steps=10) 
+        env = SumoEnv(max_steps=40,v_max=30,render_mode='human',step_length=0.2,decision_steps=5) 
     else:
         raise ValueError(f"Unknown environment: {args.env}")
 
